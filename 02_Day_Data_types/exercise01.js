a8e2aeddc4d332a5c12e7d9086506320766fc0a8
beinawair <notifier.id@gmail.com>
2022-03-18 22:59:58 +0800

    working on the second day challenge

---
+++ 02_Day_Data_types/exercise01.js
@@ -0,0 +1,18 @@
+let challenge = '30 Days Of JavaScript';
+let social = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+
+console.log(challenge);
+console.log(challenge.length);
+console.log(challenge.toUpperCase());
+console.log(challenge.toLowerCase());
+console.log(challenge.substring(3));
+console.log(challenge.substring(0, 3));
+console.log(challenge.includes('Script'));
+console.log(challenge.split(''))
+console.log(challenge.split(' '))
+console.log(social.split(','))
+console.log(challenge.replace('JavaScript', 'Python'));
+console.log(challenge.charAt(15));
+console.log(challenge.charCodeAt(11));
+console.log(challenge.indexOf('a'));
+console.log(challenge.lastIndexOf('a'));
