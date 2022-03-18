f7d542ebd2ac03bdd016454c260c9285ed4c787c
beinawair <notifier.id@gmail.com>
2022-03-18 23:10:34 +0800

    completing exercise 1 in Challenge Day-2

--- 02_Day_Data_types/exercise01.js
+++ 02_Day_Data_types/exercise01.js
@@ -1,5 +1,9 @@
 let challenge = '30 Days Of JavaScript';
+let challenge2 = ' 30 Days Of JavaScript ';
 let social = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+let word02 = 'You cannot end a sentence with because because because is a conjunction';
+let word1 = '30 Days of '
+let word2 = 'JavaScript'
 
 console.log(challenge);
 console.log(challenge.length);
@@ -16,3 +20,12 @@ console.log(challenge.charAt(15));
 console.log(challenge.charCodeAt(11));
 console.log(challenge.indexOf('a'));
 console.log(challenge.lastIndexOf('a'));
+console.log(word02.indexOf('because'));
+console.log(word02.lastIndexOf('because'));
+console.log(word02.search('because'));
+console.log(challenge2.trim(' '));
+console.log(challenge.startsWith(''));
+console.log(challenge2.endsWith(' '));
+console.log(challenge.match('a'));
+console.log(word1.concat(word2));
+console.log(challenge.repeat(2));
